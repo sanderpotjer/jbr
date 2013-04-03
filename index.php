0d8533ce18197a782b0f82652e9bb7e855ed61ca
Hans Kuijpers <info@hkweb.nl>
2013-04-03 17:36:52 +0300

    Update index.php
    verwijdering van overbodige info in html-tag

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ JFactory::getDocument()->addStyleSheet('templates/'.$this->template.'/css/templa
 ?>
 
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<jdoc:include type="head" />
